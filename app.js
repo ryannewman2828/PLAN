fc6515f252eee75e0f3c6e85fb758338009b2aac
Ryan Newman <ryannewman2828@gmail.com>
2016-07-05 21:52:10 -0400

    database configuration
    Installed mongoose and started configuring the database for the users

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var db = require('./config/db');
+
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -40,10 +44,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.render('error');
   });
 }
 
@@ -51,10 +52,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
 
 
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb://username:password@ds015335.mlab.com:15335/userbase'
+}
