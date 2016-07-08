f4b194f602ea53eeeddfa52d845104df5a3fc95e
Ryan Newman <ryannewman2828@gmail.com>
2016-07-07 21:25:46 -0400

    Added jsonWebToken
    Added the JsonWebToken to the users model

--- server/model/users.js
+++ server/model/users.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var DAY_IN_WEEK = 7;
 
 var userSchema = new mongoose.Schema({
 	email: {
@@ -18,12 +21,25 @@ var userSchema = new mongoose.Schema({
 userSchema.methods.setPassword = function(password){
 	this.salt = crypto.randomBytes(16).toString('hex');
 	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
-}
+};
 
 
 userSchema.methods.validPassword = function(password) {
 	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 	return this.hash === hash;
-}
+};
+
+userSchema.methods.generateJwt = function () {
+	var expiry = new Date();
+	expiry.setDate(expiry.getDate() + DAY_IN_WEEK);
+
+	// TODO: Move SECRET to an external file or the database itself
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: parseInt(expiry.getTime() / 1000)
+	}, "SECRET");
+};
 
 module.exports = mongoose.model('User', userSchema);
