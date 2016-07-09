921df73f1399d54a8b73e48437367957520c5f23
Ryan Newman <ryannewman2828@gmail.com>
2016-07-09 16:44:51 -0400

    Added gulp
    Added the framework and dependencies to be able to use gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var gulpConcat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+gulp.task('scripts', function () {
+
+});
+
+gulp.task('sass', function () {
+
+});
+
+gulp.task('watch', function () {
+
+});
+
+gulp.task('default', ['scripts', 'sass', 'watch']);
