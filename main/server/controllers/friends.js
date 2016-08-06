89f57c56705654116d1b21f6796c2e3deca80122
Ryan Newman <ryannewman2828@gmail.com>
2016-08-06 15:07:19 -0400

    Fixed pending friend bug
    Fixed a bug where if both friends were pending incorrect behaviour would
    occur, according e2e tests were added. If the friends/pending list is in
    an incorrect state during deletion/insertion instead of throwin an
    error, it fixes it silently to a correct state.

--- main/server/controllers/friends.js
+++ main/server/controllers/friends.js
@@ -28,6 +28,7 @@ module.exports.addFriend = function (req, res) {
 
 // Removes tbe friend/sender from both peoples pending lists and adds them to the friends list
 module.exports.acceptFriend = function (req, res) {
+ /*
     var sender = req.body.sender;
     User
         .findOne({username : req.params.id})
@@ -69,7 +70,44 @@ module.exports.acceptFriend = function (req, res) {
                 }
             }
         });
+*/
 
+    var sender = req.body.sender;
+    User
+        .findOne({username : req.params.id})
+        .exec(function(err, user){
+            if(err){
+                res.status(400).json(err);
+            }
+            else {
+                if (user.pendingFriends.some(function (friend) {return friend === sender;})) {
+                    user.pendingFriends.splice(user.pendingFriends.indexOf(sender), user.pendingFriends.indexOf(sender) + 1);
+                    if (!user.friends.some(function (friend) {return friend === sender;})) {
+                        user.friends.push(sender);
+                    }
+                }
+                user.save();
+            }
+        })
+        .then(function () {
+            User
+                .findOne({username : sender})
+                .exec(function(err, user2){
+                    if(err){
+                        res.status(400).json(err);
+                    }
+                    else {
+                        if (user2.pendingFriends.some(function (friend) {return friend === req.params.id;})) {
+                            user2.pendingFriends.splice(user2.pendingFriends.indexOf(req.params.id), user2.pendingFriends.indexOf(req.params.id) + 1);
+                        }
+                        if (!user2.friends.some(function (friend) {return friend === req.params.id;})) {
+                            user2.friends.push(req.params.id);
+                        }
+                        user2.save();
+                        res.status(200).json({message : 'request went through'});
+                    }
+                });
+        });
 };
 
 //removes from the users pending list
@@ -108,7 +146,8 @@ module.exports.deleteFriend = function (req, res) {
                 }
                 user.save();
             }
-        }).then(function () {
+        })
+        .then(function () {
             User
                 .findOne({username : sender})
                 .exec(function(err, user2){
--- test/friend.spec.js
+++ test/friend.spec.js
@@ -34,7 +34,7 @@ describe('Friend', function() {
     afterEach(function () {
         User.remove({}, function () {});
     });
-    it('should send, accept,view a friend request and delete it successfully', function() {
+    it('should send, accept, view a friend request and delete it successfully', function() {
         browser.get('/login')
             .then(function () {
                 element(by.model('credentials.email')).clear();
@@ -104,7 +104,7 @@ describe('Friend', function() {
                 });
             });
     });
-    it('should allow both people to delete the other ', function () {
+    it('should allow both people to delete the other', function () {
         var browser2 = browser.forkNewDriverInstance();
         browser.get('/login')
             .then(function () {
@@ -153,6 +153,184 @@ describe('Friend', function() {
                 browser2.element.all(by.repeater('friend in user.friends')).then(function(items) {
                     expect(items.length).toBe(0);
                 });
+                browser2.quit();
+            });
+    });
+    it('should allow both people to be pending and reject each other', function () {
+        var browser2 = browser.forkNewDriverInstance();
+        browser.get('/login')
+            .then(function () {
+                element(by.model('credentials.email')).clear();
+                element(by.model('credentials.email')).sendKeys('fake@gmail.com');
+                element(by.model('credentials.password')).clear();
+                element(by.model('credentials.password')).sendKeys('fake');
+                element(by.css("button.btn.btn-default")).click();
+                browser.waitForAngular();
+                browser.get('/profile/secret2');
+            })
+            .then(function () {
+                element(by.css('[ng-click="addFriend()"]')).click();
+                browser.wait(function() {
+                    return browser.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser.switchTo().alert().accept();
+                browser.get('/profile');
+            })
+            .then(function () {
+                browser2.get('login');
+                browser2.element(by.model('credentials.email')).clear();
+                browser2.element(by.model('credentials.email')).sendKeys('fake2@gmail.com');
+                browser2.element(by.model('credentials.password')).clear();
+                browser2.element(by.model('credentials.password')).sendKeys('fake');
+                browser2.element(by.css("button.btn.btn-default")).click();
+                browser2.waitForAngular();
+                browser2.get('/profile/secret');
+            })
+            .then(function () {
+                browser2.element(by.css('[ng-click="addFriend()"]')).click();
+                browser2.wait(function() {
+                    return browser2.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser2.switchTo().alert().accept();
+                browser2.get('/profile');
+                browser.get('/profile');
+            })
+            .then(function () {
+                browser2.element.all(by.repeater('friend in user.pendingFriends')).get(0).element(by.css('[ng-click="rejectFriend(friend)"]')).click();
+                browser2.waitForAngular();
+                element.all(by.repeater('friend in user.pendingFriends')).get(0).element(by.css('[ng-click="rejectFriend(friend)"]')).click();
+                browser.waitForAngular();
+                element.all(by.repeater('friend in user.friends')).then(function(items) {
+                    expect(items.length).toBe(0);
+                });
+                browser2.element.all(by.repeater('friend in user.friends')).then(function(items) {
+                    expect(items.length).toBe(0);
+                });
+                browser2.quit();
+            });
+    });
+    it('should allow both people to be pending and one accept the other', function () {
+        var browser2 = browser.forkNewDriverInstance();
+        browser.get('/login')
+            .then(function () {
+                element(by.model('credentials.email')).clear();
+                element(by.model('credentials.email')).sendKeys('fake@gmail.com');
+                element(by.model('credentials.password')).clear();
+                element(by.model('credentials.password')).sendKeys('fake');
+                element(by.css("button.btn.btn-default")).click();
+                browser.waitForAngular();
+                browser.get('/profile/secret2');
+            })
+            .then(function () {
+                element(by.css('[ng-click="addFriend()"]')).click();
+                browser.wait(function() {
+                    return browser.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser.switchTo().alert().accept();
+                browser.get('/profile');
+            })
+            .then(function () {
+                browser2.get('login');
+                browser2.element(by.model('credentials.email')).clear();
+                browser2.element(by.model('credentials.email')).sendKeys('fake2@gmail.com');
+                browser2.element(by.model('credentials.password')).clear();
+                browser2.element(by.model('credentials.password')).sendKeys('fake');
+                browser2.element(by.css("button.btn.btn-default")).click();
+                browser2.waitForAngular();
+                browser2.get('/profile/secret');
+            })
+            .then(function () {
+                browser2.element(by.css('[ng-click="addFriend()"]')).click();
+                browser2.wait(function() {
+                    return browser2.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser2.switchTo().alert().accept();
+                browser2.get('/profile');
+                browser.get('/profile');
+            })
+            .then(function () {
+                element.all(by.repeater('friend in user.pendingFriends')).get(0).element(by.css('[ng-click="acceptFriend(friend)"]')).click();
+                browser.waitForAngular();
+                browser2.get('/profile');
+                browser2.waitForAngular();
+                return [element.all(by.repeater('friend in user.friends')).get(0).element(by.xpath('/html/body/div/div/div[3]/ul[2]/li/a')).getText(),
+                    browser2.element.all(by.repeater('friend in user.friends')).get(0).element(by.xpath('/html/body/div/div/div[3]/ul[2]/li/a')).getText()];
+            })
+            .then(function (friends) {
+                expect(friends[0]).toEqual('secret2');
+                expect(friends[1]).toEqual('secret');
+                browser2.quit();
+            });
+    });
+    it('should allow both people to be pending and accept each other', function () {
+        var browser2 = browser.forkNewDriverInstance();
+        browser.get('/login')
+            .then(function () {
+                element(by.model('credentials.email')).clear();
+                element(by.model('credentials.email')).sendKeys('fake@gmail.com');
+                element(by.model('credentials.password')).clear();
+                element(by.model('credentials.password')).sendKeys('fake');
+                element(by.css("button.btn.btn-default")).click();
+                browser.waitForAngular();
+                browser.get('/profile/secret2');
+            })
+            .then(function () {
+                element(by.css('[ng-click="addFriend()"]')).click();
+                browser.wait(function() {
+                    return browser.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser.switchTo().alert().accept();
+                browser.get('/profile');
+            })
+            .then(function () {
+                browser2.get('login');
+                browser2.element(by.model('credentials.email')).clear();
+                browser2.element(by.model('credentials.email')).sendKeys('fake2@gmail.com');
+                browser2.element(by.model('credentials.password')).clear();
+                browser2.element(by.model('credentials.password')).sendKeys('fake');
+                browser2.element(by.css("button.btn.btn-default")).click();
+                browser2.waitForAngular();
+                browser2.get('/profile/secret');
+            })
+            .then(function () {
+                browser2.element(by.css('[ng-click="addFriend()"]')).click();
+                browser2.wait(function() {
+                    return browser2.switchTo().alert().then(
+                        function() { return true; },
+                        function() { return false; }
+                    );
+                });
+                browser2.switchTo().alert().accept();
+                browser2.get('/profile');
+                browser.get('/profile');
+            })
+            .then(function () {
+                browser2.element.all(by.repeater('friend in user.pendingFriends')).get(0).element(by.css('[ng-click="acceptFriend(friend)"]')).click();
+                browser2.waitForAngular();
+                element.all(by.repeater('friend in user.pendingFriends')).get(0).element(by.css('[ng-click="acceptFriend(friend)"]')).click();
+                browser.waitForAngular();
+                return [element.all(by.repeater('friend in user.friends')).get(0).element(by.xpath('/html/body/div/div/div[3]/ul[2]/li/a')).getText(),
+                    browser2.element.all(by.repeater('friend in user.friends')).get(0).element(by.xpath('/html/body/div/div/div[3]/ul[2]/li/a')).getText()];
+            })
+            .then(function (friends) {
+                expect(friends[0]).toEqual('secret2');
+                expect(friends[1]).toEqual('secret');
+                browser2.quit();
             });
     });
 });
